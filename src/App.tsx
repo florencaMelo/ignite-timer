5e394c8dafa1ec535c86a6f07a9149e4b9a64a6a
Florença de Melo Junqueira <jflor.melo@gmail.com>
2023-05-05 10:26:28 -0300

    subindo alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { ThemeProvider } from "styled-components"
+
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/themes/global"
+
 export function App() {
   return (
-    <h1>OI teste</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>OI teste</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
