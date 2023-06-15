9154ab3e0e5e245d0b28d83a6747ecefbdf142c9
Florença de Melo Junqueira <jflor.melo@gmail.com>
2023-06-15 20:12:52 -0300

    subindo página Home e estilizações
    a página Home possui o timer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,33 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+
 export function Home() {
-  return <h1>Home</h1>
+  return(
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label>Vou trabalhar em</label>
+          <input id="task" />
+
+          <label>Durante</label>
+          <input type="number" id="minutesAmound" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24}/>
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
--- src/styles/themes/global.ts
+++ src/styles/themes/global.ts
@@ -7,7 +7,19 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :focus{
+    outline: 0;
+    box-sizing: 0 0 0 2px ${props => props.theme["green-500"]};
+  }
+
   body{
-    color: #333;
+    background: ${props => props.theme["gray-900"]};
+    color:${props => props.theme["gray-300"]}
+  }
+  
+  body, input-security, textarea, button {
+    font-family: 'Roboto';
+    font-weight: 400;
+    font-size: 1rem;
   }
 `
